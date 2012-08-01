ea30f4e3e221303c67af42f07aa8ff9518643264
eroy <ericsroy@gmail.com>
2012-08-01 11:35:13 -0700

    libraries

---
+++ leptonica-1.6.9/src/freetype.h
@@ -0,0 +1,40 @@
+/*====================================================================*
+ -  Copyright (C) 2008 Leptonica.  All rights reserved.
+ -  This software is distributed in the hope that it will be
+ -  useful, but with NO WARRANTY OF ANY KIND.
+ -  No author or distributor accepts responsibility to anyone for the
+ -  consequences of using this software, or for whether it serves any
+ -  particular purpose or works at all, unless he or she says so in
+ -  writing.  Everyone is granted permission to copy, modify and
+ -  redistribute this source code, for commercial or non-commercial
+ -  purposes, with the following restrictions: (1) the origin of this
+ -  source code must not be misrepresented; (2) modified versions must
+ -  be plainly marked as such; and (3) this notice may not be removed
+ -  or altered from any source or modified source distribution.
+ *====================================================================*/
+
+#ifndef  LEPTONICA_FREETYPE_H
+#define  LEPTONICA_FREETYPE_H
+
+#define  LEPTONICA_FT_RESOLUTION 96
+
+typedef struct ft_library_st FT_LIBRARY;
+
+LEPT_DLL extern FT_LIBRARY *ftInitLibrary(void);
+
+LEPT_DLL extern void ftShutdownLibrary(FT_LIBRARY  *lib_ptr);
+
+LEPT_DLL extern PIX *pixWriteTTFText(FT_LIBRARY  *lib_ptr,
+	PIX         *pixs,
+	l_float32    size,
+	l_float32    angle,
+	l_int32      x,
+	l_int32      y,
+	l_int32      letter_space,
+	l_uint32     color,
+	l_uint8     *fontfile,
+	l_uint8     *text,
+	l_int32      text_len,
+	l_int32     *brect);
+
+#endif  /* LEPTONICA_FREETYPE_H */
